d22b4b1c585ffba18a859ada62c9cb86b71c2557
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2021-05-27 13:04:07 +0300

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,16 @@
-import * as React from "react";
-import { Button, Input, Flex, Checkbox, Heading } from "@chakra-ui/react";
+import * as React from 'react';
+import { Button, Input, Flex, Checkbox, Heading } from '@chakra-ui/react';
+
+import { useStore } from 'effector-react';
+
+import $store, { setNewTodo, addTodo } from '../store';
 
 function TodoListItems() {
+  const store = useStore($store);
+
   return (
     <>
-      {[].map((todo: { id: number; text: string }) => (
+      {store.todos.map((todo) => (
         <Flex pt={2} key={todo.id}>
           <Checkbox />
           <Input mx={2} value={todo.text} />
