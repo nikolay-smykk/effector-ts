aca6fa80c0f0f1d1f663ed9056482e11b326aab8
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2021-05-27 12:59:20 +0300

    Update TodoAdd.tsx

--- src/components/TodoAdd.tsx
+++ src/components/TodoAdd.tsx
@@ -1,11 +1,20 @@
-import * as React from "react";
-import { Button, Input, Grid } from "@chakra-ui/react";
+import * as React from 'react';
+import { Button, Input, Grid } from '@chakra-ui/react';
+import { useStore } from 'effector-react';
+
+import $store, { setNewTodo, addTodo } from '../store';
 
 function TodoAdd() {
+  const store = useStore($store);
+
   return (
     <Grid pt={2} templateColumns="5fr 1fr" columnGap="3">
-      <Input placeholder="New todo" />
-      <Button>Add Todo</Button>
+      <Input
+        placeholder="New todo"
+        valuew={store.newTodo}
+        onChange={(event) => setNewTodo(event.target.value)}
+      />
+      <Button onClick={() => addTodo()}>Add Todo</Button>
     </Grid>
   );
 }
